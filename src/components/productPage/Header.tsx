d0cbea6b42a070a479b8a7506e0a4bb67f1fda5b
kucing kalem <frosi551@gmail.com>
2025-04-15 00:34:19 +0700

    revisi

--- src/components/productPage/Header.tsx
+++ src/components/productPage/Header.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import Card from "./card";
 
 export default function Header() {
   const parallaxRef1 = useRef<HTMLDivElement>(null);
@@ -48,43 +49,16 @@ export default function Header() {
         </div>
       </div>
       <div className="flex flex-col sm:flex-row justify-between gap-x-6 mt-6 sm:mt-8">
-        <div className="flex flex-col items-center 2xl:w-[38vw] mb-12 md:mb-28 lg:w-[39.09vw] h-auto bg-white rounded-4xl overflow-hidden box-shadow">
-          <div className="w-full px-5 pb-2 pt-5">
-            <h1 className="2xl:text-3xl lg:text-xl xl:text-2xl font-bold text-blue-800 font-cabin">
-              Meningkatkan efisiensi bisnis perusahaan
-            </h1>
-            <p className="lg:text-base xl:text-lg 2xl:text-xl mt-2 text-blue-950 font-cabin">
-              Talenta Digital membantu perusahaan Anda mencapai efisiensi
-              operasional yang optimal melalui integrasi teknologi terdepan.
-            </p>
-          </div>
-          <div className="w-full h-full mt-3">
-            <img
-              src="/assets/icons/img-1.png"
-              alt="card"
-              className="w-full object-fill"
-            />
-          </div>
-        </div>
-        {/* card2 */}
-        <div className="flex flex-col items-center 2xl:w-[38vw] mb-12 md:mb-28 lg:w-[39.09vw] h-auto bg-white rounded-4xl overflow-hidden box-shadow">
-          <div className="w-full px-5 pb-2 pt-5">
-            <h1 className="2xl:text-3xl lg:text-xl xl:text-2xl font-bold text-blue-800 font-cabin">
-              Meningkatkan efisiensi bisnis perusahaan
-            </h1>
-            <p className="lg:text-base xl:text-lg 2xl:text-xl mt-2 text-blue-950 font-cabin">
-              Talenta Digital membantu perusahaan Anda mencapai efisiensi
-              operasional yang optimal melalui integrasi teknologi terdepan.
-            </p>
-          </div>
-          <div className="w-full h-full mt-3">
-            <img
-              src="/assets/icons/img-1.png"
-              alt="card"
-              className="w-full object-fill"
-            />
-          </div>
-        </div>
+          <Card
+            judul="Meningkatkan efisiensi bisnis perusahaan"
+            isi="Talenta Digital membantu perusahaan Anda mencapai efisiensi operasional yang optimal melalui integrasi teknologi terdepan."
+            img="/assets/icons/img-1.png"
+          />
+          <Card
+            judul="Meningkatkan efisiensi bisnis perusahaan"
+            isi="Talenta Digital menawarkan solusi lengkap dirancang untuk menyederhanakan proses bisnis dan mengoptimalkan kinerja perusahaan."
+            img="/assets/icons/img-1.png"
+          />
       </div>
     </div>
   );
