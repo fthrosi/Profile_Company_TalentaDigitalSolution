fe53ab2f5812c09e27da75e277a8be5e2ba7a6eb
kucing kalem <frosi551@gmail.com>
2025-04-15 00:47:35 +0700

    revisi

--- src/components/portofolio/Technology.tsx
+++ src/components/portofolio/Technology.tsx
@@ -4,29 +4,29 @@ const Teknologi = () => {
       <div className="w-full lg:w-1/2 flex flex-col items-center lg:items-end order-2 lg:order-1 ">
         <div className="flex md:gap-20 gap-10 justify-between">
           <div className="flex flex-col gap-y-5 items-center">
-            <img src="/assets/icons/NodeJs.png" alt=""  className="size-10"/>
+            <img src="/assets/icons/NodeJs.png" alt=""  className="size-10 2xl:size-30"/>
             <h1 className="font-cabin text-blue-950 font-semibold">Node JS</h1>
           </div>
           <div className="flex flex-col gap-y-5 items-center">
-            <img src="/assets/icons/flutter.png" alt="" className="size-10" />
+            <img src="/assets/icons/flutter.png" alt="" className="size-10 2xl:size-30" />
             <h1 className="font-cabin text-blue-950 font-semibold">Flutter</h1>
           </div>
           <div className="flex flex-col gap-y-5 items-center">
-            <img src="/assets/icons/wordpress.png" alt="" className="size-10" />
+            <img src="/assets/icons/wordpress.png" alt="" className="size-10 2xl:size-30" />
             <h1 className="font-cabin text-blue-950 font-semibold">Wordpress</h1>
           </div>
         </div>
         <div className="flex md:gap-20 gap-10 mt-10 justify-around">
           <div className="flex flex-col gap-y-5 items-center">
-            <img src="/assets/icons/Kotlin.png" alt=""  className="size-10"/>
+            <img src="/assets/icons/Kotlin.png" alt=""  className="size-10 2xl:size-30"/>
             <h1 className="font-cabin text-blue-950 font-semibold">Kotlin</h1>
           </div>
           <div className="flex flex-col gap-y-5 items-center">
-            <img src="/assets/icons/React.png" alt="" className="size-10" />
+            <img src="/assets/icons/React.png" alt="" className="size-10 2xl:size-30" />
             <h1 className="font-cabin text-blue-950 font-semibold">React JS</h1>
           </div>
           <div className="flex flex-col gap-y-5 items-center">
-            <img src="/assets/icons/tensorflow.png" alt="" className="size-10" />
+            <img src="/assets/icons/tensorflow.png" alt="" className="size-10 2xl:size-30" />
             <h1 className="font-cabin text-blue-950 font-semibold">Tensorflow</h1>
           </div>
         </div>
--- src/components/productPage/card.tsx
+++ src/components/productPage/card.tsx
@@ -6,11 +6,11 @@ interface cardProps {
 const Card: React.FC<cardProps> = ({ judul, isi, img }) => {
   return (
     <div className="flex flex-col items-center 2xl:w-[38vw] mb-12 md:mb-28 lg:w-[39.09vw] h-auto bg-white rounded-4xl overflow-hidden box-shadow">
-      <div className="w-full px-5 pb-2 pt-5">
+      <div className="w-full px-5 pb-2 pt-5 lg:h-1/3">
         <h1 className="2xl:text-3xl lg:text-xl xl:text-2xl font-bold text-blue-800 font-cabin">{judul}</h1>
         <p className="lg:text-base xl:text-lg 2xl:text-xl mt-2 text-blue-950 font-cabin">{isi}</p>
       </div>
-      <div className="w-full h-full mt-3">
+      <div className="w-full lg:h-2/3 mt-3">
         <img src={img} alt="card" className="w-full h-full object-fill" />
       </div>
     </div>
